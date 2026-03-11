52404693df332086ca950b2a6453480a2d8a2ac7
VERGHOTE GUY <GUY.VERGHOTE@saint-remi.net>
2026-03-11 12:06:08 +0100

    11/03

--- Interface/FrmVisiteAjout.cs
+++ Interface/FrmVisiteAjout.cs
@@ -16,9 +16,61 @@ namespace Interface
             InitializeComponent();
         }
 
+        # region procédures événementielles
+
+
         private void FrmVisiteAjout_Load(object sender, EventArgs e)
         {
-            this.lblTitre.Text = "Ajouter une visite";
+            parametrerComposant();
+          
         }
+
+        #endregion
+
+        private void parametrerComposant()
+        {
+            this.lblTitre.Text = "Ajouter une visite";
+
+            // alimentation de la zone de liste des praticiens
+            foreach (Praticien unPraticien in session.MesPraticiens)
+            {
+                cbxPraticien.Items.Add(unPraticien);
+            }
+            cbxPraticien.DisplayMember = "NomPrenom";
+            cbxPraticien.ValueMember = "Id";
+            cbxPraticien.SelectedIndex = 0;
+
+            // alimentation de la zone de liste des motifs
+            cbxMotif.DataSource = session.LesMotifs;
+            cbxMotif.DisplayMember = "Libelle";
+            cbxMotif.ValueMember = "Id";
+
+
+        }
+
+        //parametrage DataGridView
+        parameterDgv(dgVisites);
+
+        #region procédures
+
+        private void parametrerComposant()
+        
+
+        private void parametreDgv(DataGridView dgv)
+
+        private void remplirDgv()
+        {
+            //vider le datagridview
+            dgvVisites.Rows.Clear();
+
+            //parcourir les visites de la session par ordre chronologique
+            foreach(Visite v in session.MesVisites.Where(v => v.Bilan is null))
+            {
+                dgvVisites.Rows.Add(v.DateEtHeure, v.DateEtHeure)
+            }
+        }
+
+        #endregion
+
     }
 }
