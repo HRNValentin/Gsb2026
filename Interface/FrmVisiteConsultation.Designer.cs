01a4abbf43557b1f53349418015dc9ad152cbd23
vhernu <valentin.hernu@saint-remi.net>
2026-04-02 08:15:45 +0200

    02/14

---
+++ Interface/FrmVisiteConsultation.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Interface
+{
+    partial class FrmVisiteConsultation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmVisiteConsultation));
+            panelCentral = new Panel();
+            label1 = new Label();
+            lstMedicament = new ListBox();
+            dgvEchantillon = new DataGridView();
+            lblBilan = new Label();
+            lblMotif = new Label();
+            Praticien = new Label();
+            dgvVisites = new DataGridView();
+            panelPraticien = new Panel();
+            lblSpecialite = new Label();
+            lblType = new Label();
+            lblEmail = new Label();
+            lblTelephone = new Label();
+            lblRue = new Label();
+            lblPraticien = new Label();
+            panelCentral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvEchantillon).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvVisites).BeginInit();
+            panelPraticien.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblTitre
+            // 
+            lblTitre.Size = new Size(1025, 74);
+            // 
+            // panelCentral
+            // 
+            panelCentral.Anchor = AnchorStyles.None;
+            panelCentral.Controls.Add(label1);
+            panelCentral.Controls.Add(lstMedicament);
+            panelCentral.Controls.Add(dgvEchantillon);
+            panelCentral.Controls.Add(lblBilan);
+            panelCentral.Controls.Add(lblMotif);
+            panelCentral.Controls.Add(Praticien);
+            panelCentral.Controls.Add(dgvVisites);
+            panelCentral.Controls.Add(panelPraticien);
+            panelCentral.Location = new Point(0, 82);
+            panelCentral.Name = "panelCentral";
+            panelCentral.Size = new Size(1025, 382);
+            panelCentral.TabIndex = 13;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(337, 280);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 15);
+            label1.TabIndex = 11;
+            label1.Text = "Médicaments présentés";
+            // 
+            // lstMedicament
+            // 
+            lstMedicament.FormattingEnabled = true;
+            lstMedicament.Location = new Point(337, 298);
+            lstMedicament.Name = "lstMedicament";
+            lstMedicament.Size = new Size(324, 64);
+            lstMedicament.TabIndex = 10;
+            // 
+            // dgvEchantillon
+            // 
+            dgvEchantillon.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEchantillon.Location = new Point(803, 212);
+            dgvEchantillon.Name = "dgvEchantillon";
+            dgvEchantillon.Size = new Size(200, 157);
+            dgvEchantillon.TabIndex = 9;
+            // 
+            // lblBilan
+            // 
+            lblBilan.AutoSize = true;
+            lblBilan.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblBilan.Location = new Point(330, 193);
+            lblBilan.Name = "lblBilan";
+            lblBilan.Size = new Size(96, 15);
+            lblBilan.TabIndex = 8;
+            lblBilan.Text = "Bilan de la visite";
+            // 
+            // lblMotif
+            // 
+            lblMotif.AutoSize = true;
+            lblMotif.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblMotif.Location = new Point(334, 169);
+            lblMotif.Name = "lblMotif";
+            lblMotif.Size = new Size(38, 15);
+            lblMotif.TabIndex = 7;
+            lblMotif.Text = "Motif";
+            // 
+            // Praticien
+            // 
+            Praticien.AutoSize = true;
+            Praticien.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Praticien.Location = new Point(330, 19);
+            Praticien.Name = "Praticien";
+            Praticien.Size = new Size(56, 15);
+            Praticien.TabIndex = 6;
+            Praticien.Text = "Praticien";
+            // 
+            // dgvVisites
+            // 
+            dgvVisites.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvVisites.Location = new Point(12, 19);
+            dgvVisites.MinimumSize = new Size(0, 50);
+            dgvVisites.Name = "dgvVisites";
+            dgvVisites.Size = new Size(316, 350);
+            dgvVisites.TabIndex = 2;
+            // 
+            // panelPraticien
+            // 
+            panelPraticien.BorderStyle = BorderStyle.FixedSingle;
+            panelPraticien.Controls.Add(lblSpecialite);
+            panelPraticien.Controls.Add(lblType);
+            panelPraticien.Controls.Add(lblEmail);
+            panelPraticien.Controls.Add(lblTelephone);
+            panelPraticien.Controls.Add(lblRue);
+            panelPraticien.Controls.Add(lblPraticien);
+            panelPraticien.Location = new Point(334, 48);
+            panelPraticien.Name = "panelPraticien";
+            panelPraticien.Size = new Size(683, 118);
+            panelPraticien.TabIndex = 1;
+            // 
+            // lblSpecialite
+            // 
+            lblSpecialite.Location = new Point(303, 41);
+            lblSpecialite.Name = "lblSpecialite";
+            lblSpecialite.Size = new Size(38, 15);
+            lblSpecialite.TabIndex = 5;
+            lblSpecialite.Text = "label1";
+            // 
+            // lblType
+            // 
+            lblType.Location = new Point(303, 9);
+            lblType.Name = "lblType";
+            lblType.Size = new Size(38, 15);
+            lblType.TabIndex = 4;
+            lblType.Text = "label1";
+            // 
+            // lblEmail
+            // 
+            lblEmail.Location = new Point(22, 92);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(38, 15);
+            lblEmail.TabIndex = 3;
+            lblEmail.Text = "label1";
+            // 
+            // lblTelephone
+            // 
+            lblTelephone.Location = new Point(22, 68);
+            lblTelephone.Name = "lblTelephone";
+            lblTelephone.Size = new Size(38, 15);
+            lblTelephone.TabIndex = 2;
+            lblTelephone.Text = "label1";
+            // 
+            // lblRue
+            // 
+            lblRue.Location = new Point(22, 41);
+            lblRue.Name = "lblRue";
+            lblRue.Size = new Size(38, 15);
+            lblRue.TabIndex = 1;
+            lblRue.Text = "label1";
+            // 
+            // lblPraticien
+            // 
+            lblPraticien.Location = new Point(22, 9);
+            lblPraticien.Name = "lblPraticien";
+            lblPraticien.Size = new Size(38, 15);
+            lblPraticien.TabIndex = 0;
+            lblPraticien.Text = "label1";
+            // 
+            // FrmVisiteConsultation
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1025, 512);
+            Controls.Add(panelCentral);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            KeyPreview = true;
+            Location = new Point(0, 0);
+            Name = "FrmVisiteConsultation";
+            Text = "FrmVisiteConsultation";
+            Controls.SetChildIndex(lblTitre, 0);
+            Controls.SetChildIndex(panelCentral, 0);
+            panelCentral.ResumeLayout(false);
+            panelCentral.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvEchantillon).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvVisites).EndInit();
+            panelPraticien.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panelCentral;
+        private Panel panelPraticien;
+        private Label lblPraticien;
+        private DataGridView dgvVisites;
+        private Label lblSpecialite;
+        private Label lblType;
+        private Label lblEmail;
+        private Label lblTelephone;
+        private Label lblRue;
+        private Label Praticien;
+        private Label lblMotif;
+        private ListBox lstMedicament;
+        private DataGridView dgvEchantillon;
+        private Label lblBilan;
+        private Label label1;
+    }
+}
---
+++ Interface/FrmVisiteConsultation.cs
@@ -0,0 +1,384 @@
+﻿using Metier;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Interface
+{
+    public partial class FrmVisiteConsultation : FrmBase
+    {
+        public FrmVisiteConsultation(Session uneSession) : base(uneSession)
+        {
+            InitializeComponent();
+            centrerFormulaire();
+            this.Load += FrmVisiteConsultation_Load;
+            this.Resize += FrmVisiteConsultation_Resize;
+        }
+
+        private void FrmVisiteConsultation_Load(object? sender, EventArgs e)
+        {
+            parametrerComposant();
+            centrerFormulaire();
+            remplirDgvVisites();
+        }
+
+        private void FrmVisiteConsultation_Resize(object? sender, EventArgs e)
+        {
+            centrerFormulaire();
+        }
+
+        private void centrerFormulaire()
+        {
+            panelCentral.Left = (this.ClientSize.Width - panelCentral.Width) / 2;
+        }
+
+        private void dgvVisites_SelectionChanged(object sender, EventArgs e)
+        {
+            afficher();
+        }
+
+        private void parametrerComposant()
+        {
+
+            // titre de la fenêtre
+            this.lblTitre.Text = "Consultation des visites";
+            centrerFormulaire();
+
+            // paramétrage du datagridview
+            parametrerDgvEchantillon();
+
+            parametrerDgv(dgvVisites);
+        }
+
+        private void parametrerDgvEchantillon()
+        {
+            dgvEchantillon.Enabled = false;
+
+            //style général 
+            dgvEchantillon.Font = new Font("Segoe UI", 10);
+            dgvEchantillon.RowTemplate.Height = 28;
+            dgvEchantillon.BorderStyle = BorderStyle.FixedSingle;
+
+            //bordures des cellules
+            dgvEchantillon.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgvEchantillon.GridColor = Color.LightGray;
+
+            //suppression selection visuelle
+            dgvEchantillon.DefaultCellStyle.SelectionBackColor = Color.White;
+            dgvEchantillon.DefaultCellStyle.SelectionForeColor = Color.Black;
+
+            // entete visible et stylé
+
+            dgvEchantillon.ColumnHeadersVisible = true;
+            dgvEchantillon.EnableHeadersVisualStyles = false;
+            dgvEchantillon.ColumnHeadersDefaultCellStyle.BackColor = Color.WhiteSmoke;
+            dgvEchantillon.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
+            dgvEchantillon.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            dgvEchantillon.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+
+            //colonnes
+
+            dgvEchantillon.Columns.Clear();
+
+            //colonne nom 
+            DataGridViewColumn col = new DataGridViewTextBoxColumn();
+            col.Name = "Nom";
+            col.HeaderText = "Nom";
+            col.Width = 200;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+
+            //colonne quantité
+            DataGridViewColumn colQte = new DataGridViewTextBoxColumn();
+            colQte.Name = "Quantite";
+            colQte.HeaderText = "Quantité";
+            colQte.Width = 100;
+            colQte.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            // ajout des colonnes datagridview
+            dgvEchantillon.Columns.Add(col);
+            dgvEchantillon.Columns.Add(colQte);
+
+            // ajustement automatique des colonnes
+            dgvEchantillon.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // ajustement largeur totale
+            dgvEchantillon.Width = getLargeur(dgvEchantillon);
+        }
+        private void parametrerDgv(DataGridView dgv)
+        {
+            // initialisation du datagridview : supprimerRendezVous des colonnes et des lignes ajoutées par défaut dans le composant
+            dgv.Columns.Clear();
+            dgv.Rows.Clear();
+
+            #region paramètrage concernant le datagridview dans son ensemble
+
+            // Accessibilité (doit être sur true si on veut pouvoir utiliser les barres de défilement)
+            dgv.Enabled = true;
+
+            // style de bordure
+            dgv.BorderStyle = BorderStyle.FixedSingle;
+
+            // couleur de fond 
+            dgv.BackgroundColor = Color.White;
+
+            // couleur de texte  
+            dgv.ForeColor = Color.Black;
+
+            // police de caractères par défaut
+            dgv.DefaultCellStyle.Font = new Font("Georgia", 11);
+
+            // mode de sélection dans le composant : FullRowSelect, CellSelect ...
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            // sélection multiple 
+            dgv.MultiSelect = false;
+
+            // l'utilisateur peut-il ajouter ou supprimer des lignes ?
+            dgv.AllowUserToDeleteRows = false;
+            dgv.AllowUserToAddRows = false;
+
+            // L'utilisateur peut-il modifier le contenu des cellules ou est-elle réservée à la programmation ?
+            dgv.EditMode = DataGridViewEditMode.EditProgrammatically;
+
+            // l'utilisateur peut-il redimensionner les colonnes et les lignes ?
+            dgv.AllowUserToResizeColumns = false;
+            dgv.AllowUserToResizeRows = false;
+
+            // l'utilisateur peut-il modifier l'ordre des colonnes ?
+            dgv.AllowUserToOrderColumns = false;
+
+            // le composant accepte t'il le 'déposer' dans un Glisser - Déposer ?
+            dgv.AllowDrop = false;
+
+            // Largeur : à contrôler avec la largeur des colonnes si elle est définie
+            // dgv.Width = 700;
+
+            // faut-il ajuster automatiquement la taille des colonnes par un ajustement proportionnel à la largeur totale (commenter la ligne si non)
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            #endregion
+
+            #region paramètrage concernant la ligne d'entête 
+
+            // visibilité
+            dgv.ColumnHeadersVisible = true;
+
+            // bordure
+            dgv.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+
+            // style  [à adapter] (ici : noir sur fond transparent sans mise en évidence de la sélection)
+            dgv.EnableHeadersVisualStyles = false;
+            DataGridViewCellStyle style = dgv.ColumnHeadersDefaultCellStyle;
+            style.BackColor = Color.WhiteSmoke;
+            style.ForeColor = Color.Black;
+            style.SelectionBackColor = Color.WhiteSmoke;    // même couleur que backColor pour ne pas mettre en évidence la colonne sélectionnée
+            style.SelectionForeColor = Color.Black;
+            style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            style.Font = new Font("Georgia", 12, FontStyle.Bold);
+
+
+            // hauteur 
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dgv.ColumnHeadersHeight = 40;
+
+            #endregion
+
+            #region paramètrage concernant l'entête de ligne (la colonne d'entête ou le sélecteur)
+
+            // visible 
+            dgv.RowHeadersVisible = false;
+
+            // style de bordure  
+            dgv.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+
+
+            #endregion
+
+            #region paramètrage au niveau des lignes
+
+            // Hauteur 
+            dgv.RowTemplate.Height = 30;
+
+            #endregion
+
+            #region paramètrage au niveau des cellules
+
+            // style de bordure 
+            dgv.CellBorderStyle = DataGridViewCellBorderStyle.None;
+
+            // couleur de fond, ne pas utiliser transparent car la couleur de la ligne sélectionnée restera après sélection
+            dgv.RowsDefaultCellStyle.BackColor = Color.White;
+
+            // Couleur alternative appliquée une ligne sur deux
+            // unDgv.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(255, 238, 238, 238);
+
+            #endregion
+
+            #region paramètrage au niveau de la zone sélectionnée
+
+            // couleur de fond mettre la même que les cellules si on ne veut pas mettre la zone en évidence 
+            dgv.RowsDefaultCellStyle.SelectionBackColor = System.Drawing.Color.White;
+
+            // couleur du texte
+            dgv.RowsDefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+
+            #endregion
+
+            #region paramètrage des colonnes
+
+            // faut-il ajuster automatiquement la taille des colonnes par un ajustement proportionnel à la largeur totale (commenter la ligne si non)
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+
+
+            // description de chaque colonne  [partie à personnaliser] 
+            DataGridViewColumn col;
+
+            // la première colonne contient l'objet visite : elle est masquée
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Visite";
+            col.HeaderText = "";
+            col.Visible = false;
+            dgv.Columns.Add(col);
+
+
+            // Colonne 0 : La date de la visite
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Date";
+            col.HeaderText = "Programmée le";
+            col.Width = 200;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.Columns.Add(col);
+
+            // Colonne 1 :  heure du rendez-vous
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Heure";
+            col.HeaderText = "à";
+            col.Width = 50;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.Columns.Add(col);
+
+            // Colonne 2 ! ville du rendez-vous
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Lieu";
+            col.HeaderText = "sur";
+            col.Width = 200;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.Columns.Add(col);
+
+            // Colonne 3 : praticien (nom et prénom)
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Praticien";
+            col.HeaderText = "Praticien";
+            col.Width = 200;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.Columns.Add(col);
+
+
+            // faut-il désactiver le tri sur toutes les colonnes ? (commenter les lignes si non)
+            for (int i = 0; i < dgv.ColumnCount; i++)
+                dgv.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+        }
+        private int getLargeur(DataGridView dgv)
+        {
+            int largeur = 0;
+            foreach (DataGridViewColumn col in dgv.Columns)
+            {
+                if (col.Visible)
+                    largeur += col.Width;
+            }
+            if (dgv.RowHeadersVisible)
+                largeur += dgv.RowHeadersWidth;
+
+            return largeur + 2; // 2 pixels de marge
+        }
+            private void remplirDgvVisites()
+        {
+            // vider le datagridview
+            dgvVisites.Rows.Clear();
+
+            // Parcourir les visites dans l'ordre chronologique
+            foreach (Visite v in session.MesVisites)
+            {
+                // add the Visite object in the hidden first column followed by display values
+                int rowIndex = dgvVisites.Rows.Add(
+                         v,
+                         v.DateEtHeure.ToLongDateString(),
+                         v.DateEtHeure.ToShortTimeString(),
+                         v.LePraticien.Ville,
+                         v.LePraticien.NomPrenom);
+
+                if (v.Bilan != null)
+                {
+                    // color the displayed cells in green when a bilan exists
+                    var cells = dgvVisites.Rows[rowIndex].Cells;
+                    // parcourir uniquement les cellules réellement présentes (sauter la colonne cachée index 0)
+                    for (int i = 1; i < cells.Count; i++)
+                    {
+                        cells[i].Style.ForeColor = Color.Green;
+                    }
+                }
+            }
+            // afficher les détails de la première visite
+            afficher();
+        }
+        // afficher la visite sélectionnée dans le datagridview
+        private void afficher()
+        {
+            if (dgvVisites.Rows.Count == 0 || dgvVisites.SelectedRows.Count == 0)
+            {
+                ViderAffichage();
+                return;
+            }
+
+            // afficher les détails de la visite dans les composants prévus à cet effet
+            Visite uneVisite = (Visite)dgvVisites.SelectedRows[0].Cells[0].Value!;
+
+            lblPraticien.Text = uneVisite.LePraticien.NomPrenom;
+            lblEmail.Text = uneVisite.LePraticien.Email;
+            lblRue.Text = uneVisite.LePraticien.Rue;
+            lblTelephone.Text = uneVisite.LePraticien.Telephone;
+            lblType.Text = uneVisite.LePraticien.Type!.Libelle;
+            lblSpecialite.Text = uneVisite.LePraticien.Specialite!.Libelle;
+            lblMotif.Text = uneVisite.LeMotif.Libelle;
+            lblBilan.Text = uneVisite.Bilan == null ? string.Empty : uneVisite.Bilan;
+            lstMedicament.Items.Clear();
+            if (uneVisite.PremierMedicament != null)
+                lstMedicament.Items.Add(uneVisite.PremierMedicament.Nom);
+            if (uneVisite.SecondMedicament != null)
+                lstMedicament.Items.Add(uneVisite.SecondMedicament.Nom);
+            // remplir le datagridview des échantillons remis lors de la visite
+            dgvEchantillon.Rows.Clear();
+            foreach (KeyValuePair<Medicament, int> unCouple in uneVisite)
+            {
+                Medicament unMedicament = unCouple.Key;
+                int qte = unCouple.Value;
+                dgvEchantillon.Rows.Add(unMedicament.Nom, qte.ToString());
+            }
+        }
+            private void ViderAffichage()
+            {
+                lblPraticien.Text = string.Empty;
+                lblEmail.Text = string.Empty;
+                lblRue.Text = string.Empty;
+                lblTelephone.Text = string.Empty;
+                lblType.Text = string.Empty;
+                lblSpecialite.Text = string.Empty;
+                lblMotif.Text = string.Empty;
+                lblBilan.Text = string.Empty;
+                lstMedicament.Items.Clear();
+                dgvEchantillon.Rows.Clear();
+
+
+        }
+
+        }
+
+
+}
+    #endregion
